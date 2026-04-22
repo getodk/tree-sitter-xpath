f908a95c83b1deb809f06d8a14f156b46c3b991e
Gareth Bowen <gbowen@getodk.org>
2026-04-23 08:37:56 +1200

    feat(#1): setup to run as separate project

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # @getodk/tree-sitter-xpath
 
-...
+XPath grammar for tree-sitter
+
+## Contributing to the Project
+
+### Requirements
+
+- [Volta](https://volta.sh/) to ensure consistent `node` and `yarn` versions.
+- [docker](https://www.docker.com/)
+
+### Running locally
+
+```sh
+yarn
+yarn build
+yarn test
+```
---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+	printWidth: 100,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: true,
+	overrides: [
+		{
+			files: ['.*', '*.json', '*.md', '*.toml', '*.yml'],
+			options: {
+				useTabs: false,
+			},
+		},
+	],
+};
